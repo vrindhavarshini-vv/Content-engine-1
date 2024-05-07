46d8ef309a832a3efc3d9323ab4f8c75a0d9e571
Anish-Krishnan347 <anishkrishnan347@gmail.com>
2024-05-07 16:17:13 +0530

    fix:redux-usingInTemplatePage

--- src/Routes/store.js
+++ src/Routes/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AdminLogin from "./Slices/adminLogin";
+import templateSlice from "./Slices/templateSlice";
 
 export default configureStore({
   reducer: {
-    adminLogin: AdminLogin
+    adminLogin: AdminLogin,
+    template : templateSlice
   },
 });
