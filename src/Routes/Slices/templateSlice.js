3450db47412cf396aa7d9e20f1301e5490d9f8ec
Anish-Krishnan347 <anishkrishnan347@gmail.com>
2024-05-09 16:04:21 +0530

    fix:stop api call after click of the category

--- src/Routes/Slices/templateSlice.js
+++ src/Routes/Slices/templateSlice.js
@@ -1,25 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const TemplateSlice = createSlice(
-    {
-        name:'template',
-        initialState:{
-            fbCategory:[],
-            selectedCategory:null,
-            fbType:[]
-        },
-        reducers:{
-            setFbCategory:(state,action)=>{
-                state.fbCategory = action.payload
-            },
-            setSelectedCategory:(state,action)=>{
-                state.selectedCategory = action.payload
-            },
-            setFbType:(state,action)=>{
-                state.fbType = action.payload
-            }
-        }
-    }
-)
-export const {setFbCategory,setSelectedCategory,setFbType} = TemplateSlice.actions
-export default TemplateSlice.reducer
+export const TemplateSlice = createSlice({
+  name: "template",
+  initialState: {
+    fbCategory: [],
+    selectedCategory: null,
+    fbType: [],
+  },
+  reducers: {
+    setFbCategory: (state, action) => {
+      state.fbCategory = action.payload;
+    },
+    setSelectedCategory: (state, action) => {
+      state.selectedCategory = action.payload;
+    },
+    setFbType: (state, action) => {
+      state.fbType = action.payload;
+    },
+  },
+});
+export const { setFbCategory, setSelectedCategory, setFbType } =
+  TemplateSlice.actions;
+export default TemplateSlice.reducer;
